5e106c1a41f9e13a9a9c1129c753f0f8b0164509
VBhoy <83490034+V-bhoy@users.noreply.github.com>
2025-04-12 13:02:23 +0530

    use @Qualifier annotation

--- annotation-based-autowiring/src/main/java/com/controller/BeansController.java
+++ annotation-based-autowiring/src/main/java/com/controller/BeansController.java
@@ -9,11 +9,19 @@ public class BeansController {
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         // When using annotations by default is disabled to enable it, use context:annotation-config
         // in xml
+
         // using @Autowired annotation on field for address entity
         // field-injection is used,  it is directly injecting the dependency into the field
         // resulting in tight coupling, hard to test
+        // it injects the bean by Type
+        // if multiple beans of same type are present it throws exception
+        // @Qualifier annotation is used with value attribute which tells spring to inject
+        // the bean that matches the value with bean name, if no bean is found, it throws
+        // exception
+
         // using @Autowired annotation on setter method for course entity
         // spring calls setter method for injecting course (setter injection)
+
         // using @Autowired annotation on constructor for department entity
         // spring calls constructor method for injecting department (constructor injection)
         Employee e1 = context.getBean("e1", Employee.class);
--- annotation-based-autowiring/src/main/java/com/entity/Employee.java
+++ annotation-based-autowiring/src/main/java/com/entity/Employee.java
@@ -1,11 +1,13 @@
 package com.entity;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
     private String name;
 
     @Autowired
+    @Qualifier(value = "address2")
     private Address address;
     private Course course;
     private Department department;
